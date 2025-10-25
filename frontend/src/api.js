1eb514efedb6174768160fa6d9f8269ad6f1bfe0
ragul-rajendran18 <ragul18rs@gmail.com>
2025-10-25 09:45:31 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://ecom-project-r7ob.vercel.app/api' });
 
 export const fetchProducts = () => API.get('/products').then(r => r.data);
 export const checkout = (items) => API.post('/cart/checkout', { items }).then(r => r.data);
